c013390fe5c0d6dc3dc7ac587791a34037c7ba13
sierrahun97 <jp200801@hotmail.com>
2025-11-04 23:08:00 -0500

    Ajustes archivos no necesarios.

--- src/interfaces/servidor.ts
+++ src/interfaces/servidor.ts
@@ -9,23 +9,21 @@ import { ActualizarProyecto } from '../aplicacion/casosUso/ActualizarProyecto';
 import { EliminarProyecto } from '../aplicacion/casosUso/EliminarProyecto';
 
 
-// La función ahora solo crea el servidor base y la conexión BD.
 export const crearServidorBase = (): FastifyInstance => {
     const servidor = fastify({ logger: true });
     configurarConexionBD(servidor);
     return servidor;
 }
 
-// Creamos una nueva función para configurar todas las rutas e inyectar dependencias.
+
 export const configurarRutas = (servidor: FastifyInstance, repositorioProyectos: IRepositorioProyecto) => {
-    // Inicialización de Casos de Uso con el repositorio inyectado
     const crearProyecto = new CrearProyecto(repositorioProyectos);
     const listarProyectos = new ListarProyectos(repositorioProyectos);
     const obtenerProyectoPorId = new ObtenerProyectoPorId(repositorioProyectos);
     const actualizarProyecto = new ActualizarProyecto(repositorioProyectos);
     const eliminarProyecto = new EliminarProyecto(repositorioProyectos);
 
-    // Registro de rutas, inyectando los casos de uso
+    // Registro de rutas
     servidor.register(proyectoRutas(
         crearProyecto, 
         listarProyectos, 
