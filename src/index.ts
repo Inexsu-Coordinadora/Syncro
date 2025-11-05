3258a9dd18ab5c49f1578a09c34cef5acdb38a53
sierrahun97 <jp200801@hotmail.com>
2025-11-05 00:09:22 -0500

    Feat: actualización servidor.ts para optimizar el contenido y las redes. index.ts unificado como inicializador del proyecto.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,42 @@
+import { crearServidorBase } from './interfaces/servidor';
 import { RepositorioProyectoPostgres } from './infraestructura/repositorios/repositorioProyectoPostgres';
-import { crearServidorBase, configurarRutas } from './interfaces/servidor';
+import { proyectoRutas } from './interfaces/rutas/proyectoRutas';
+import { CrearProyecto } from './aplicacion/casosUso/CrearProyecto';
+import { ListarProyectos } from './aplicacion/casosUso/ListarProyectos';
+import { ObtenerProyectoPorId } from './aplicacion/casosUso/ObtenerProyectoPorId';
+import { ActualizarProyecto } from './aplicacion/casosUso/ActualizarProyecto';
+import { EliminarProyecto } from './aplicacion/casosUso/EliminarProyecto';
 import * as dotenv from 'dotenv';
 
-const PUERTO = process.env.PUERTO || 3000;
+
 dotenv.config();
+const PUERTO = process.env.PUERTO || 3000;
 const start = async () => {
   try {
     const servidor = crearServidorBase();
     const repositorioProyectos = new RepositorioProyectoPostgres(servidor);
+    const crear = new CrearProyecto(repositorioProyectos);
+    const consultarTodos = new ListarProyectos(repositorioProyectos);
+    const consultarPorId = new ObtenerProyectoPorId(repositorioProyectos);
+    const actualizar = new ActualizarProyecto(repositorioProyectos);
+    const eliminar = new EliminarProyecto(repositorioProyectos);
 
-    configurarRutas(servidor, repositorioProyectos);
+
+    servidor.register(proyectoRutas(
+        crear, 
+        consultarTodos, 
+        consultarPorId,
+        actualizar,
+        eliminar
+    ), { prefix: '/api/proyectos' });
+
+
+
     await servidor.listen({ port: Number(PUERTO) });
     console.log(`Servidor de Proyectos iniciado en http://localhost:${PUERTO}`);
+
   } catch (err: any) {
-    console.error(`Fallo al iniciar el servidor: ${err.message}`);
+    console.error(`[ERROR] Fallo al iniciar el servidor: ${err.message}`);
     process.exit(1);
   }
 };
