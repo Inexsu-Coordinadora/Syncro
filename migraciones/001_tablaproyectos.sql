17c287876f01c7c83710850fc3289b1c2e5623bc
Maria Paula <mariapaulaescobar65@gmail.com>
2025-11-09 21:40:50 -0500

    migraciones/tablaproyectos actualizada

--- migraciones/001_tablaproyectos.sql
+++ migraciones/001_tablaproyectos.sql
@@ -1,13 +1,13 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS proyectos (
-    "idProyecto" SERIAL PRIMARY KEY,
-    "nombreProyecto" VARCHAR(100) NOT NULL,
-    "descripcionProyecto" TEXT,
-    "clienteId" VARCHAR(50) NOT NULL,
-    "fecha_inicio" DATE,
-    "fecha_fin" DATE,
-    "estadoProyecto" VARCHAR(50) NOT NULL,
-    "consultor_asignado" VARCHAR(100),
-    "roles_definidos" TEXT
+    id_proyecto UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    codigo_proyecto VARCHAR(50) UNIQUE NOT NULL,
+    nombre_proyecto VARCHAR(150) NOT NULL,
+    descripcion_proyecto TEXT,
+    fecha_inicio DATE NOT NULL,
+    fecha_fin DATE,
+    estado_proyecto VARCHAR(50) NOT NULL,
+    id_cliente UUID NOT NULL,
+    FOREIGN KEY (id_cliente) REFERENCES clientes(id_cliente)
 );
