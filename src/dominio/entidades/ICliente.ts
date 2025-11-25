03a9df90d47c77b48de3497f6c2caf4cb56a3870
sierrahun97 <jp200801@hotmail.com>
2025-11-25 12:11:06 -0500

    Ejecución y correción de package.json

---
+++ src/dominio/entidades/ICliente.ts
@@ -0,0 +1,8 @@
+export interface ICliente {
+    idCliente?: string;
+    nombreCliente: string;
+    emailCliente: string;
+    telefonoCliente: string;
+    direccionCliente: string;
+    empresaCliente?: string;
+}
