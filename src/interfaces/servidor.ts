88c55da21071e0957936d24624d70ed4424e3461
sierrahun97 <jp200801@hotmail.com>
2025-11-26 01:13:25 -0500

    actualización servidor.ts y package.json

--- src/interfaces/servidor.ts
+++ src/interfaces/servidor.ts
@@ -1,31 +1,6 @@
 import Fastify, { FastifyInstance } from "fastify";
-import consultorRutas from "../interfaces/rutas/consultorRutas";
 
-<<<<<<< HEAD
-export const crearServidorBase = async (): Promise<FastifyInstance> => {
-    const servidor = fastify({ 
-        logger: true,
-        ajv: {
-            customOptions: {
-                removeAdditional: 'all',
-                coerceTypes: true
-            }
-        }
-    });
-
-    // Configurar la base de datos
-    await configurarConexionBD(servidor);
-
-    return servidor;
-}
-=======
 export function crearServidorBase(): FastifyInstance {
     const app = Fastify({ logger: true });
-
-    app.register(consultorRutas, { prefix: "/api" });
-
-    
-
     return app;
 }
->>>>>>> b30b58e262039a921993cc4e13d2a9ce26e55470
