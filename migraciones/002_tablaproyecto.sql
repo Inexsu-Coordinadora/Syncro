06cbda82344a9f6d170057689acd39b5cffcd89e
sierrahun97 <jp200801@hotmail.com>
2025-11-26 10:03:19 -0500

    correcion tablas sql

--- migraciones/002_tablaproyecto.sql
+++ migraciones/002_tablaproyecto.sql
@@ -1,11 +1,11 @@
-CREATE TABLE proyectos (
-    idProyecto SERIAL PRIMARY KEY,
-    nombreProyecto VARCHAR(100) NOT NULL,
-    descripcionProyecto TEXT,
-    clienteId VARCHAR(50) NOT NULL,
-    fecha_inicio DATE,
-    fecha_fin DATE,
-    estadoProyecto VARCHAR(50) NOT NULL,
-    consultor_asignado VARCHAR(100),
-    roles_definidos TEXT
+CREATE TABLE "proyectos" (
+  "idProyecto" SERIAL PRIMARY KEY,
+  "nombreProyecto" VARCHAR(255) NOT NULL,
+  "descripcionProyecto" TEXT,
+  "clienteId" VARCHAR(50),
+  "fechaInicio" DATE NOT NULL,
+  "fechaFin" DATE,
+  "estadoProyecto" VARCHAR(50) NOT NULL,
+  "consultorAsignado" VARCHAR(255),
+  "rolesDefinidos" TEXT[]
 );
